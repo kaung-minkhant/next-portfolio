bebf24623415faddaba995ef52efafe626881e95
kaung min khant <kaungminkhant.official@gmail.como>
2024-03-04 00:37:23 +0630

    added base code

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import HeroSession from "@/components/HeroSection";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col bg-[#121212] container mx-auto px-12 py-4">
+      <HeroSession />
+    </main>
+  );
+}
---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HeroSession = () => {
+  return (
+    <section>
+        <h1 className='mb-4 text-4xl lg:text-6xl font-extrabold'>Hi, I'm Kaung Min Khant</h1>
+        <p className='text-lg lg:text-xl'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem consectetur rem ex ducimus aspernatur. Quisquam eligendi ea fuga odit aut dolore mollitia non doloribus, facere quod magnam nesciunt. Doloribus, eius.</p>
+    </section>
+  )
+}
+
+export default HeroSession
